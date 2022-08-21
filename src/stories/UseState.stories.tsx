f1bb6e8ad49a44c7a2af79eed6552af69bb8801a
Maxim Remez <remezmaxim1992@gmail.com>
2022-08-22 01:12:19 +0300

    fancy useState

---
+++ src/stories/UseState.stories.tsx
@@ -0,0 +1,22 @@
+import React, {useState} from "react"
+
+export default {
+    title: "UseState"
+}
+
+export const Example1ForUseState = () => {
+    console.log("Example1")
+    const [counter, setCounter] = useState<number>(() => {
+        console.log("initialValue")
+        return 0
+    })
+    const changer = (state: number) => {
+        return state + 1
+    }
+
+    return <div>
+        <button onClick={() => setCounter(state => state + 1)}>+</button>
+        {counter}
+    </div>
+
+}

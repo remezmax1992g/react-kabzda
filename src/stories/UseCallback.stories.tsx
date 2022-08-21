6081982b2698b94da657644e522fe53c5eac4152
Maxim Remez <remezmaxim1992@gmail.com>
2022-08-22 00:40:10 +0300

    useCallback

---
+++ src/stories/UseCallback.stories.tsx
@@ -0,0 +1,34 @@
+import React, {useCallback, useMemo, useState} from "react"
+
+export default {
+    title: "useCallback"
+}
+
+
+const SecretBooks = (props: {addBook: () => void }) => {
+    console.log("SECRETBooks")
+    return <div>
+        <button onClick={() => props.addBook()}>add book</button>
+    </div>
+}
+const Books = React.memo(SecretBooks)
+export const LikeUseCallback = () => {
+    console.log("LikeUseCallback")
+    const [count, setCount] = useState<number>(0)
+    const [books, setBooks] = useState<Array<string>>(["React", "JS", "HTML", "CSS"])
+    let memoizedAddBook = useMemo(() => {
+        return () => {
+            setBooks([...books, "Angular" + new Date().getTime()])}}, [books])
+    let memoizedAddBook2 = useCallback(() => {
+            setBooks([...books, "Angular" + new Date().getTime()])}, [books])
+    return (
+        <div>
+            <button onClick={() => {
+                setCount(count + 1)
+            }}>+
+            </button>
+            {count}
+            <Books addBook={memoizedAddBook2}/>
+        </div>
+    )
+}
